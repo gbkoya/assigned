313ccf63705234138ad7df845d2d68e98d4adc26
gbkoya <107157016+gbkoya@users.noreply.github.com>
2022-06-14 09:39:24 +0100

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>Task Manager</h1>
+      <img  src="download.jpg" alt="" width="72" height="57">
+      </img>   
+    </div>
+  )
+}
+
+export default Header
